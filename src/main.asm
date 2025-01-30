657736b801cf93146fa041490e8e1711d2a038e4
pitriche <ptrichet@live.fr>
2025-01-30 18:34:00 +0100

    grub timeout 0, clear screen added

--- src/main.asm
+++ src/main.asm
@@ -7,12 +7,36 @@ bits 32
 start:
 	mov esp, stack_end	; setup the stack pointer
 
-	; screeen is 80 2 byte characters wide and 25 chars tall
+	call clear_screen				; write spaces to all of the screen buffer
+
+	; screeen is 80 2 byte characters wide and 25 chars tall, write 42 in the middle
 	push (80 * 2) * 12 + (39 * 2)	; push the argument into stack
 	call write_42					; call write 42 with offset as argument
 	add esp, 4						; remove the argument from stack
 
 	hlt
+
+clear_screen:
+	push ebp					; save the C stack frame
+	mov ebp, esp
+
+	mov dl, 0x20
+	mov dh, 0x1f
+	mov ecx, 0
+	.loop:
+		mov eax, ecx
+		shl eax, 1		; counter * 2
+		add eax, 0xb8000; VGA array address
+
+		mov word [eax], dx
+
+		inc ecx
+		cmp ecx, 80 * 25
+		jne .loop
+
+	mov esp, ebp				; restore the C stack frame
+	pop ebp
+	ret
 
 ; load 42 in screen memory, at the offset specified in the first argument
 write_42:
